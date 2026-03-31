261528c5c0d2a2c7d3f5a515c0d2c4dce80f963b
Mamy Lainé Soumaoro <lainemamysoumaoro@gmail.com>
2026-03-31 02:16:05 +0000

    feat: Create migrations (user, project, task) and throw

---
+++ api/migrations/20260331014453-create-task.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+   await queryInterface.createTable('Tasks', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+
+      projectId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'Projects',
+          key: 'id'
+        }
+      },
+
+        userId: {
+          type: Sequelize.UUID,
+          references: {
+            model: 'Users',
+            key: 'id'
+          }
+        },
+        
+        createAt: Sequelize.DATE,
+        updatedAt: Sequelize.DATE
+
+
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+   await queryInterface.dropTable('Tasks'); 
+  }
+};
