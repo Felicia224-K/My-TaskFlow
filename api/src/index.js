f6461b6b8274a035a6324e7f49b0a222ddb8f1ca
Mamy Lainé Soumaoro <lainemamysoumaoro@gmail.com>
2026-03-31 03:02:24 +0000

    feat: Authenticate registration and login in authcontroller and  authroute , update index.js, try postman and it worked

--- api/src/index.js
+++ api/src/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const {connectDB, sequelize}   = require('./config/database');
 require('dotenv').config();
 
-
+const authRoutes = require('./routes/authRoutes');
 const app = express();
 
 
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 connectDB();
 
@@ -19,7 +21,7 @@ app.get('/', (req, res) => {
     res.send('This is the API for the project');
 });
 
-const port =  3000;
+const port =  4000;
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
