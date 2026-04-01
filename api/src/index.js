4ba4c850d555815544aa063942068d4fe2711577
Mamy Lainé Soumaoro <lainemamysoumaoro@gmail.com>
2026-04-01 10:58:53 +0000

    IMPLEMENT: install swagger, update index, test url

--- api/src/index.js
+++ api/src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const {connectDB, sequelize}   = require('./config/database');
 require('dotenv').config();
 
@@ -20,6 +22,32 @@ app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/dashboard', dashboardRoutes);
+
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Taskflow API',
+            version: '1.0.0',
+            description: 'API documentation for Taskflow',
+        },
+        servers: [
+            { 
+                url: 'http://localhost:4000', description: 'Local server'
+            }
+        ],
+    },
+
+    apis: [
+        './src/routes/*.js', './src/models/*.js'
+    ], // Path to the API docs
+};
+
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 
 connectDB();
 
