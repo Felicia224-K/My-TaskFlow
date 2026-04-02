3048cf466268e083825f636415050c4cbe1965b0
Mamy Lainé Soumaoro <lainemamysoumaoro@gmail.com>
2026-04-02 10:11:40 +0000

    Implement Dashboard

--- frontend/taskflow-project/src/pages/Dashboard.jsx
+++ frontend/taskflow-project/src/pages/Dashboard.jsx
@@ -1,6 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import api from '../services/api';
+
+
 function Dashboard() {
+
+    const [projects, setProjects] = useState([]);
+    const [stats, setStats] = useState(null);
+
+
+    useEffect(() => {
+        api.get('/projects')
+            .then(res => setProjects(res.data))
+            .catch(err => console.log(err));
+
+        api.get('/dashboard')
+            .then(res => setStats(res.data.data))
+            .catch(err => console.log(err));
+    }, []);
 
     useEffect(() => {
         api.get('/projects')
@@ -9,7 +25,27 @@ function Dashboard() {
     }, []);
 
     return (
-        <h1>Dashboard</h1>
+        <div>
+            <h2>Dashboard</h2>
+
+            {/*Stats*/}
+            {stats && (
+                <div>
+                    <p>Total Projects: {stats.totalProjects}</p>
+                    <p>Total Tasks: {stats.totalTasks} </p>
+                    <p>Completed Tasks: {stats.completedTasks} </p>
+                </div>
+            )}
+
+            {/*Projects*/}
+            <h3>Projects</h3>
+            {projects.map(p => (
+                <div key={p.id}>
+                    <h4> { p.name} </h4>
+                </div>
+            ))}
+
+       </div> 
     );
 }
 
